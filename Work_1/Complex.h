c507e2c5c057dff46fdc293c7b632ed6593f3700
zumhad <zumhad@gmail.com>
2022-04-26 23:05:29 +0300

    work_1

--- Work_1/Complex.h
+++ Work_1/Complex.h
@@ -8,6 +8,9 @@ public:
 	Complex() : m_re(0.0), m_im(0.0) {}
 	Complex(double re, double im) : m_re(re), m_im(im) {}
 
+	void Set(double re, double im) { m_re = re; m_im = im; }
+	double Abs() { return sqrt(m_re * m_re + m_im * m_im); }
+
 	friend std::ostream& operator<< (std::ostream& out, const Complex& c);
 	friend Complex operator+ (const Complex& c1, const Complex& c2);
 	friend Complex operator- (const Complex& c1, const Complex& c2);
--- Work_1/Main.cpp
+++ Work_1/Main.cpp
@@ -1,16 +1,39 @@
-
 #include "Complex.h"
+#include <fstream>
 using namespace std;
 
 int main()
 {
-	Complex c1(5, 3);
-	Complex c2(0.33, -1);
+	ifstream file("complex.txt");
 
-	cout << c1 + c2 << endl;
-	cout << c1 - c2 << endl;
-	cout << c1 * c2 << endl;
-	cout << c1 / c2 << endl;
+	if (!file.is_open())
+	{
+		cout << "error" << endl;
+		return 0;
+	}
 
+	int n; file >> n;
+
+	Complex* p = new Complex[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		double re, im;
+		file >> re; file >> im;
+		p[i].Set(re, im);
+	}
+
+	Complex* maxP = &(p[0]);
+	for (int i = 1; i < n; i++)
+	{
+		if (p[i].Abs() > maxP->Abs())
+			maxP = &(p[i]);
+	}
+
+	cout << *maxP << endl;
+
+	delete[] p;
+
+	file.close();
 	return 0;
 }
